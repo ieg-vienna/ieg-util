b00822d1457ee1b165dcd14bc1f7bf57cacf75a2
Alexander Rind <rind@ifs.tuwien.ac.at>
2011-09-07 15:14:17 +0000

    move migrated source files to common project

---
+++ src/util/xml/JaxbMarshaller.java
@@ -0,0 +1,99 @@
+package visuexplore.persistence;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+/**
+ * Xml serializer with <a href="https://jaxb.dev.java.net">jaxb</a>
+ */
+public class JaxbMarshaller {
+
+	/**
+	 * loads an object from an XML file. The XML file has to be in UTF-8.
+	 * 
+	 * @param xmlFile
+	 * @param clazz
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public static Object load(String xmlFile, Class clazz) {
+		Object model = null;
+		try {
+			model = loadUser(xmlFile, clazz);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+		return model;
+	}
+
+	/**
+	 * loads an object from an XML file without catching exceptions. 
+	 * The XML file has to be in UTF-8.
+	 * 
+	 * @param xmlFile
+	 * @param clazz
+	 * @return
+	 * @throws IOException
+	 * @throws JAXBException
+	 */
+	@SuppressWarnings("unchecked")
+	public static Object loadUser(String xmlFile, Class clazz)
+			throws IOException, JAXBException {
+		Reader reader;
+		Object model = null;
+		reader = new BufferedReader(new InputStreamReader(new FileInputStream(
+				xmlFile), "UTF-8"));
+
+		JAXBContext context;
+
+		context = JAXBContext.newInstance(clazz);
+
+		Unmarshaller m;
+
+		m = context.createUnmarshaller();
+		model = m.unmarshal(reader);
+
+		reader.close();
+
+		return model;
+	}
+
+	/**
+	 * serializes an object and writes it to an XML file. The XML file is always
+	 * in UTF-8.
+	 * 
+	 * @param xmlFile
+	 * @param obj
+	 */
+	public static void save(String xmlFile, Object o) {
+		try {
+			Writer writer = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream(xmlFile), "UTF-8"));
+			JAXBContext context = JAXBContext.newInstance(o.getClass());
+			Marshaller m = context.createMarshaller();
+			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+			m.marshal(o, writer);
+			writer.close();
+
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/util/xml/LinkedHashMapXmlAdapter.java
@@ -0,0 +1,64 @@
+package visuexplore.persistence;
+
+import java.util.*;
+
+import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+/**
+ * generic mapping of a HashMap for JAXB (experimental) 
+ * @author Alex Rind
+ *
+ * @param <K>
+ * @param <V>
+ */
+public class LinkedHashMapXmlAdapter<K, V> extends
+		XmlAdapter<LinkedHashMapXmlAdapter.MarshalledForm<K, V>, LinkedHashMap<K, V>> {
+
+	@Override
+	public MarshalledForm<K, V> marshal(LinkedHashMap<K, V> v) throws Exception {
+		MarshalledForm<K, V> result = new MarshalledForm<K, V>();
+
+		for (Map.Entry<K, V> var : v.entrySet()) {
+			result.entries.add(new KeyValuePair<K, V>(var.getKey(), 
+					var.getValue()));
+		}
+
+		return result;
+	}
+
+	@Override
+	public LinkedHashMap<K, V> unmarshal(MarshalledForm<K, V> v)
+			throws Exception {
+		LinkedHashMap<K, V> result = new LinkedHashMap<K, V>();
+
+		for (KeyValuePair<K, V> var : v.entries) {
+			result.put(var.m_key, var.m_value);
+		}
+
+		return result;
+	}
+
+	static class MarshalledForm<K, V> {
+		@XmlElement(name = "option")
+		LinkedList<KeyValuePair<K, V>> entries = new LinkedList<KeyValuePair<K, V>>();
+	}
+
+	static class KeyValuePair<K, V> {
+		@XmlElement
+		// @XmlAttribute(required = true)
+		K m_key;
+
+		@XmlElement
+		// @XmlValue
+		V m_value;
+
+		public KeyValuePair() {
+		}
+
+		KeyValuePair(K key, V value) {
+			this.m_key = key;
+			this.m_value = value;
+		}
+	}
+}
